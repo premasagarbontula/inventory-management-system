8b1b9078dc47a2428be3b99b54b445030e90ef56
Prema Sagar Bontula <137033028+premasagarbontula@users.noreply.github.com>
2026-06-14 18:59:30 +0530

    configure cookies for production deployment

--- backend/controllers/userContoller.js
+++ backend/controllers/userContoller.js
@@ -70,7 +70,7 @@ const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 24 * 60 * 60 * 1000,
     });
     res.status(200).json({
